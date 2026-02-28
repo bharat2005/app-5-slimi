901dac4bb16671058b69000eb89836d202db8164
bharat2005 <bharatvdeshm2005@gmail.com>
2026-02-28 18:47:14 +0530

    refactor: Replace Column with LazyColumn in VitalsDialog
    - Replaced the `Column` composable with a `LazyColumn` inside the `VitalsDialog` to make the content scrollable.
    - Wrapped existing dialog content within `item` blocks to adapt to the `LazyColumn` structure.

--- app/src/main/java/com/ForSomeoneSpeical/app5/app_sketch/presentation/user_log_screen/components/VitalsDialog.kt
+++ app/src/main/java/com/ForSomeoneSpeical/app5/app_sketch/presentation/user_log_screen/components/VitalsDialog.kt
@@ -1,10 +1,13 @@
 package com.ForSomeoneSpeical.app5.app_sketch.presentation.user_log_screen.components
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,6 +18,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.ForSomeoneSpeical.app5.app_sketch.domain.model.DailyVitals
 import com.ForSomeoneSpeical.app5.app_sketch.domain.model.DailyVitalsDTO
 import com.ForSomeoneSpeical.app5.app_sketch.domain.model.Feeling
@@ -46,101 +51,122 @@ fun VitalsDialog(
         title = { Text("Vitals") },
         text = {
 
-            Column(
+            LazyColumn(
                 modifier = Modifier.fillMaxWidth(),
             ) {
 
-                //Body Weight
-                TextField(
-                    value = bodyWeight,
-                    onValueChange = { bodyWeight = it },
-                    placeholder = { Text("Body Weight (kg)") }
-                )
-                //Body Fat
-                TextField(
-                    value = bodyFat,
-                    onValueChange = { bodyFat = it },
-                    placeholder = { Text("Body Fat (%)") }
-                )
 
+                item {
+                    //Body Weight
+                    TextField(
+                        value = bodyWeight,
+                        onValueChange = { bodyWeight = it },
+                        placeholder = { Text("Body Weight (kg)") }
+                    )
 
-                //Physiological
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                )
-                {
-                    Text("Physiological")
-
-                    Physiological.entries.forEach { item ->
-                        Button(
-                            enabled = selectedPhysiological != item,
-                            onClick = {
-                                selectedPhysiological = item
-                            }
-                        ) { Text(item.name) }
-                    }
-
-                }
-                //Message
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                )
-                {
-                    Text("Message")
-
-                    Message.entries.forEach { item ->
-                        Button(
-                            enabled = selectedMessage != item,
-                            onClick = { selectedMessage = item }
-                        ) { Text(item.name) }
-                    }
-
-                }
-                //How are you today?
-                Column (
-                    modifier = Modifier.fillMaxWidth(),
-                )
-                {
-                    Text("How feeling today?")
-
-                    Feeling.entries.forEach { item ->
-                        Button(
-                            enabled = selectedFeeling != item,
-                            onClick = { selectedFeeling = item }
-                        ) { Text(item.name) }
-                    }
-
+                    //Body Fat
+                    TextField(
+                        value = bodyFat,
+                        onValueChange = { bodyFat = it },
+                        placeholder = { Text("Body Fat (%)") }
+                    )
                 }
 
+                item {
+                    //Physiological
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
+                    {
+                        Text("Physiological")
 
+                        Physiological.entries.forEach { item ->
+                            Button(
+                                enabled = selectedPhysiological != item,
+                                onClick = {
+                                    selectedPhysiological = item
+                                }
+                            ) { Text(item.name) }
+                        }
+
+                    }
+
+                    //Message
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
+                    {
+                        Text("Message")
+
+                        Message.entries.forEach { item ->
+                            Button(
+                                enabled = selectedMessage != item,
+                                onClick = { selectedMessage = item }
+                            ) { Text(item.name) }
+                        }
+
+                    }
+
+                    //How are you today?
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                    {
+                        Text("How feeling today?")
+
+                        Feeling.entries.forEach { item ->
+                            Button(
+                                enabled = selectedFeeling != item,
+                                onClick = { selectedFeeling = item }
+                            ) { Text(item.name) }
+                        }
+
+                    }
+                }
+
+                item {
                 //Body Measurements
-                TextField(
-                    value = chest,
-                    onValueChange = { chest = it },
-                    placeholder = { Text("Chest (cm)") }
-                )
-                TextField(
-                    value = waist,
-                    onValueChange = { waist = it },
-                    placeholder = { Text("Waist (cm)") }
-                )
-                TextField(
-                    value = hips,
-                    onValueChange = { hips = it },
-                    placeholder = { Text("Hips (cm)") }
-                )
-                TextField(
-                    value = forearms,
-                    onValueChange = { forearms = it },
-                    placeholder = { Text("Forearms (cm)") }
-                )
-                TextField(
-                    value = calf,
-                    onValueChange = { calf = it },
-                    placeholder = { Text("Calf (cm)") }
-                )
+                Column {
+                    TextField(
+                        value = chest,
+                        onValueChange = { chest = it },
+                        placeholder = { Text("Chest (cm)") }
+                    )
+                    TextField(
+                        value = waist,
+                        onValueChange = { waist = it },
+                        placeholder = { Text("Waist (cm)") }
+                    )
+                    TextField(
+                        value = hips,
+                        onValueChange = { hips = it },
+                        placeholder = { Text("Hips (cm)") }
+                    )
+                    TextField(
+                        value = forearms,
+                        onValueChange = { forearms = it },
+                        placeholder = { Text("Forearms (cm)") }
+                    )
+                    TextField(
+                        value = calf,
+                        onValueChange = { calf = it },
+                        placeholder = { Text("Calf (cm)") }
+                    )
+                }
+                }
+
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxWidth().height(500.dp).background(Color.Red)
+                    ) {
+
+                    }
+                }
+
+
+
 
             }
         },
